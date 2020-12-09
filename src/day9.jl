dce22599fae2756e85bb00941fb3d39480081e1c
MarcusTL12 <marcuspaafjellet@gmail.com>
2020-12-09 06:16:58 +0100

    Day9

---
+++ src/day9.jl
@@ -0,0 +1,50 @@
+using Base.Iterators
+
+
+function is_sum(nums, n)
+    for i in 1:length(nums)
+        for j in i + 1:length(nums)
+            if nums[i] + nums[j] == n
+                return true
+            end
+        end
+    end
+    false
+end
+
+
+function part1()
+    preamble_len = 25
+    open("inputfiles/day9/input.txt") do io
+        numbuffer = sizehint!(Int[], preamble_len)
+        lines = Iterators.Stateful(parse(Int, l) for l in eachline(io))
+        while length(numbuffer) < preamble_len
+            push!(numbuffer, first(lines))
+        end
+        n = 0
+        while (n = first(lines); is_sum(numbuffer, n))
+            popfirst!(numbuffer)
+            push!(numbuffer, n)
+        end
+        n
+    end
+end
+
+
+function part2()
+    invalid = part1()
+    
+    nums = open("inputfiles/day9/input.txt") do io
+        parse.(Int, eachline(io))
+    end
+    
+    for i in 1:length(nums)
+        for j in i + 1:length(nums)
+            if sum(@view nums[i:j]) > invalid
+                break
+            elseif sum(@view nums[i:j]) == invalid
+                return minimum(@view nums[i:j]) + maximum(@view nums[i:j])
+            end
+        end
+    end
+end
