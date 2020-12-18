8f26600b1555b82ac3c90a2f9a884cf34ba6019c
MarcusTL12 <marcuspaafjellet@gmail.com>
2020-12-18 07:17:59 +0100

    Big Brain

--- src/day18.jl
+++ src/day18.jl
@@ -1,72 +1,24 @@
-using Base.Iterators
+
+
+struct EagerInt
+    n::Int
+end
+
+
+Base.:+(a::EagerInt, b::EagerInt) = EagerInt(a.n + b.n)
+Base.:|(a::EagerInt, b::EagerInt) = EagerInt(a.n * b.n)
 
 
 function eager_eval(s)
-    acc = 0
-    i = 1
-    
-    cur_op = false
-    
-    while i <= length(s)
-        while i < length(s) && s[i] == ' '
-            i += 1
-        end
-        if i > length(s)
-            break
-        end
-        if s[i] == '('
-            i += 1
-            start = i
-            depth = 1
-            while depth > 0
-                if s[i] == '('
-                    depth += 1
-                elseif s[i] == ')'
-                    depth -= 1
-                end
-                i += 1
-            end
-            stop = i - 2
-            
-            slice = @view s[start:stop]
-            subproblem = eager_eval(slice)
-            
-            if cur_op
-                acc *= subproblem
-            else
-                acc += subproblem
-            end
-        elseif isdigit(s[i])
-            start = i
-            while i < length(s) && isdigit(s[i])
-                i += 1
-            end
-            stop = i
-            
-            num = parse(Int, String(@view s[start:stop]))
-            i += 1
-            if cur_op
-                acc *= num
-            else
-                acc += num
-            end
-        elseif s[i] in ['+', '*']
-            if s[i] == '+'
-                cur_op = false
-            else
-                cur_op = true
-            end
-            i += 1
-        end
-    end
-    
-    acc
+    s = replace(s, "*" => "|")
+    s = replace(s, r"(\d+)" => s"Main.EagerInt(\1)")
+    Meta.eval(Meta.parse(s)).n
 end
 
 
 function part1()
     open("inputfiles/day18/input.txt") do io
-        sum(eager_eval(Vector{Char}(l)) for l in eachline(io))
+        sum(eager_eval(l) for l in eachline(io))
     end
 end
 
